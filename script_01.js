0534c84d4ca38339dff10c0f826593054e13afa1
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-25 15:02:02 +0200

    Lösung ++

--- script_01.js
+++ script_01.js
@@ -1,16 +1,46 @@
 
 /*** Mini-Challenge  */
 // Satzbau + Punktuation
-
 // Vereinbarung:
 // "S" (sentence) --> "."
 // "Q" (question) --> "?"
 // "E" (exclamation) --> "!"
 
+function getSentence (arr, satzzeichen){
+
+    let gap = " ";
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i != arr.length-1) {
+            str += arr[i] + gap;
+        }
+        else{
+            if (satzzeichen === "S") {
+                str += arr[i] + "."
+            }
+            else if (satzzeichen === "Q") {
+                str += arr[i] + "?"
+            }
+            else if (satzzeichen === "E") {
+                str += arr[i] + "!"
+            }
+            else{
+                str = "Bitte gültiges Satzzeichen eingeben";  
+            }
+
+        return str; 	
+        }
+    }
+}
+
+function ausgabe(str) {
+    console.log(str);
+}
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-// ausgabe(getSentence(["Ich","bin","Peter"],"S"));
-// ausgabe(getSentence(["Bist","du","Peter"],"Q"));
-// ausgabe(getSentence(["Ich","bin"],"E"));
-// ausgabe(getSentence(["Ich","bin"],"#"));
+ausgabe(getSentence(["Ich","bin","Peters"],"S"));
+ausgabe(getSentence(["Bist","du","Peter"],"Q"));
+ausgabe(getSentence(["Ich","bin"],"E"));
+ausgabe(getSentence(["Ich","bin"],"#"));
